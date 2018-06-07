69cca476042c43ed2655804b2bab32e407b48ad4
PwnyTail <pwnytail@c3d2.de>
2018-06-07 16:58:27 +0200

    extract prepare shell script to ansible

--- prepare_local_system.sh
+++ prepare_local_system.sh
@@ -1,27 +1,15 @@
 #! /usr/bin/env bash
 
+yum update -y
+yum install -y python python-devel ansible python-pip python-wheel python-setuptools #docker-compose
 
-apt-get update
-apt-get install -y \
-         apt-transport-https \
-         ca-certificates \
-         curl \
-         gnupg2 \
-         software-properties-common \
-         dirmngr \
-         linux-headers-amd64
-curl -fsSL https://download.docker.com/linux/$(. /etc/os-release; echo "$ID")/gpg | sudo apt-key add -
-add-apt-repository \
-       "deb [arch=amd64] https://download.docker.com/linux/$(. /etc/os-release; echo "$ID") \
-       $(lsb_release -cs) stable"
-apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
-echo "deb http://ppa.launchpad.net/ansible/ansible/ubuntu xenial main" \
-    > /etc/apt/sources.list.d/ansible.list
-apt-get update
-apt-get upgrade -y
-apt-get install -y docker-ce
-systemctl enable docker
-systemctl start docker
-apt-get install -y ansible python python-pip python-dev docker-compose
-pip install --upgrade pip
-pip install ansible-container[docker] "docker<3.0"
+if [ -d "/root/.docker/config.json" ];
+then
+    rm -r /root/.docker/
+    mkdir /root/.docker
+    echo "{}" > /root/.docker/config.json
+    chown ${USER}:${USER} /root/.docker
+    chown ${USER}:${USER} /root/.docker/config.json
+    chmod g+rw /root/.docker/config.json
+fi
+
